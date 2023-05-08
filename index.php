f370d4f1a6d584921bd9766e9d142b8064501fee
SevikoA3 <31793124+SevikoA3@users.noreply.github.com>
2023-05-08 13:04:58 +0700

    bruh

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello World</title>
     <link rel="stylesheet" href="new.css">
+    <script src="testing.js">
 </head>
 <body>
     <div class="test">
