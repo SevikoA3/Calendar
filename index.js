d2c941527c5b5641995496ff339f99933841554e
SevikoA3 <31793124+SevikoA3@users.noreply.github.com>
2023-05-09 20:25:14 +0700

    minor changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let counter = 0
 function slide() {
     counter++
     if (counter % 2 == 0){
-        subPage[0].style.transform = "translateX(-100%)"
+        subPage[0].style.transform = "translateX(-101%)"
         subPage[1].style.transform = "translateX(0%)"
         subPage[2].style.transform = "translateX(0%)"
         document.getElementById("quote").innerHTML = "With Tense-Fi, productivity becomes a necessity."
@@ -13,7 +13,7 @@ function slide() {
     else {
         subPage[0].style.transform = "translateX(0%)"
         subPage[1].style.transform = "translateX(46%)"
-        subPage[2].style.transform = "translateX(100%)"
+        subPage[2].style.transform = "translateX(101%)"
         document.getElementById("quote").innerHTML = "You are welcomed to start your daily journey with Tense-Fi."
     }
 }
