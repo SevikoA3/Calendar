95f3ac6b526f634602beb9f4df5e8142649b3ff7
SevikoA3 <31793124+SevikoA3@users.noreply.github.com>
2023-05-20 14:41:27 +0700

    adding navbar

--- todo.php
+++ todo.php
@@ -27,39 +27,67 @@ $i = 1;
 </head>
 
 <form action="database/edit.php?id=<?php if(isset($_GET["id"]))echo $_GET['id']?>" method="post">
-        <section id="todoPage">
-            <div id="todoTitles">
-                <h2>TO DO</h2>
-                <div id="todoContainer">
-                    <div id="listContainer">
-                        <?php 
-                        while($row = mysqli_fetch_array($result)){
-                            $title = $row['title'];
-                        ?>
-                            <a href=todo.php?id=<?php echo $i;?>><textarea name='$i' class='todoTitle' cols='1' rows='1' placeholder='title' oninput='input1()' onclick='txtappear()'><?php echo $title ?></textarea></a>
-                        <?php $i++;} ?>
-                    </div>
-                    <a href = "database/create.php">
-                        <div class="plus" onclick="addTodo()">  
-                            <span class="material-symbols-outlined">
-                                add
-                            </span>
-                        </div>
-                    </a>
-                </div>
+    <div class="navbar">
+        <div class="navicons">
+            <div class="navicon">
+                <a href="home.php">
+                    <span class="material-symbols-outlined">
+                        home
+                    </span>
+                    <b class="navbarText" id="text1">Home</b>
+                </a>
             </div>
-            <div id="todoDescrpition">
-                <?php 
-                if(isset($_GET["id"])){
-                    $id = $_GET["id"];
-                    $searchData = "SELECT * FROM tdl WHERE id = '$id' AND username = '$username'";
-                    $resultSearch = mysqli_query($conn,$searchData);
-                    $rowData = mysqli_fetch_assoc($resultSearch);  
+            <div class="navicon">
+                <a href="todo.php">
+                    <span class="material-symbols-outlined scale-up">
+                        checklist
+                    </span>
+                    <b class="navbarText" id="text2">To-do</b>
+                </a>
+            </div>
+            <div class="navicon">
+                <a href="database/logout.php">
+                    <span class="material-symbols-outlined">
+                        door_open
+                    </span>
+                    <b class="navbarText" id="text2">Logout</b>
+                </a>
+            </div>
+        </div>
+    </div>
+    <section id="todoPage">
+        <div id="todoTitles">
+            <h2>TO DO</h2>
+            <div id="todoContainer">
+                <div id="listContainer">
+                    <?php 
+                    while($row = mysqli_fetch_array($result)){
+                        $title = $row['title'];
                     ?>
-                <textarea name='title' id='txt1' cols='1' rows='1' placeholder='title' oninput='input2()'><?php echo $rowData['title'] ?></textarea>
-                <textarea name='isi' id='txt2' cols='120' rows='20' placeholder='task' ><?php echo $rowData['isi']?></textarea>
-                <input type = 'submit' value = 'enter' id = 'txt3'>
-                <?php }?>
+                        <a href=todo.php?id=<?php echo $i;?>><textarea name='$i' class='todoTitle' cols='1' rows='1' placeholder='title' oninput='input1()' onclick='txtappear()'><?php echo $title ?></textarea></a>
+                    <?php $i++;} ?>
+                </div>
+                <a href = "database/create.php">
+                    <div class="plus" onclick="addTodo()">  
+                        <span class="material-symbols-outlined">
+                            add
+                        </span>
+                    </div>
+                </a>
             </div>
-        </section>
-    </form>
+        </div>
+        <div id="todoDescrpition">
+            <?php 
+            if(isset($_GET["id"])){
+                $id = $_GET["id"];
+                $searchData = "SELECT * FROM tdl WHERE id = '$id' AND username = '$username'";
+                $resultSearch = mysqli_query($conn,$searchData);
+                $rowData = mysqli_fetch_assoc($resultSearch);  
+                ?>
+            <textarea name='title' id='txt1' cols='1' rows='1' placeholder='title' oninput='input2()'><?php echo $rowData['title'] ?></textarea>
+            <textarea name='isi' id='txt2' cols='120' rows='20' placeholder='task' ><?php echo $rowData['isi']?></textarea>
+            <input type = 'submit' value = 'enter' id = 'txt3'>
+            <?php }?>
+        </div>
+    </section>
+</form>
