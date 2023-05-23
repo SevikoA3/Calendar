7936a4beb45c0272e1aa949b6c0495c8295727af
fnt4sia <fitraramadhan668@gmail.com>
2023-05-23 19:13:19 +0700

    yes

--- home.php
+++ home.php
@@ -18,7 +18,7 @@ $result = mysqli_query($conn, $findData);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tense-Fi</title>
-    <link rel="stylesheet" href="b.css">
+    <link rel="stylesheet" href="c.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <script defer src="todo.js"></script>
 
